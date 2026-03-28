6d6bc2d53ccf7e18ed4d5ec275d8accb923d6b82
Furkan <esquetta@gmail.com>
2026-03-28 13:24:13 +0300

    chore(web): update vulnerable dependencies

--- web/next.config.js
+++ web/next.config.js
@@ -3,13 +3,8 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL || 'http://localhost:3001',
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.API_URL || 'http://localhost:3001'}/api/:path*`,
-      },
-    ];
+  images: {
+    unoptimized: true,
   },
 };
 
